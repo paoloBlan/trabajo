dd2a2786e4a3170a5de69094e21672ad3c285985
leo Blancas <0002160013@unu.edu.pe>
2025-01-02 15:49:57 -0500

    evo-01 entity cliente

---
+++ cliente/src/main/java/com/api/cliente/Entity/Cliente.java
@@ -0,0 +1,41 @@
+package com.api.cliente.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cliente", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCliente;
+
+    @Column(length = 45)
+    private String nombre;
+
+    @Column(length = 45)
+    private String apellido;
+
+    @Column(length = 45)
+    private String telefono;
+
+    @Column(length = 45)
+    private String email;
+
+    @Column(length = 45)
+    private String ciudad;
+
+    @Column(length = 45)
+    private String pais;
+}
