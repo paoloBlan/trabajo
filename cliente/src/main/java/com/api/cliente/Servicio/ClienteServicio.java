e06aae914c7a9cf76da7f48b92ea0d9f33cff188
leo Blancas <0002160013@unu.edu.pe>
2025-01-02 21:02:08 -0500

    evo-02 corr4egido

--- cliente/src/main/java/com/api/cliente/Servicio/ClienteServicio.java
+++ cliente/src/main/java/com/api/cliente/Servicio/ClienteServicio.java
@@ -1,7 +1,7 @@
 package com.api.cliente.Servicio;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
